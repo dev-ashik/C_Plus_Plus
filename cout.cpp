bcf786fee6f42f77a4378aae1efe8a923b9579e8
MD ASHIK MAHMUD <ashik.swfu@outlook.com>
2023-02-13 00:43:13 +0800

    new line

--- cout.cpp
+++ cout.cpp
@@ -2,7 +2,13 @@
 using namespace std;
 
 int main() {
-    cout << "hello world";
-    cout << "I am learning C++";
+    // new line
+        // 1. \n ( escape sequence )
+        // 2. endl
+    cout << "hello world! \n";
+    cout << "I am learning C++\n\n";
+
+    cout << "hello world!" << endl;
+    cout << "I am learning C++\n";
     return 0;
 }
